502fc906102920eef7422842eef7a6f38497dc0a
Victor Bratko <bratko.viktor2004@gmail.com>
2022-11-13 18:56:23 +0300

    stepik_lab_3 version 1.1

--- main.cpp
+++ main.cpp
@@ -17,19 +17,17 @@ int checkInput(){
 }
 
 //выводит матрицу на экран
-void printMatrix(const int *pointer,int N){
-    for (int i = 0; i <= N - 1; i++){
-        for (int j = 0; j <= N - 1; j++){
-            printf(" %2d ",*(pointer + i*N + j));
-        }
-        cout << "\n";
+void printMatrix(int *pointer,int N ,int *end){
+    for (int *i = pointer, count = 1; i <= end; count++, i++){
+        printf("%2d ", *i);
+        if (count % N == 0){ cout << "\n" ;}
     }
     cout << "\n";
 }
 
 //Используя арифметику указателей, заполняет квадратичную целочисленную матрицу порядка N (6,8,10) случайными числами от 1 до  N*N согласно схемам, приведенным на рисунках.
 // Пользователь должен видеть процесс заполнения квадратичной матрицы.
-void function1(int *pointer, int N){
+void function1(int *pointer,int N, int *end){
     char exercise;
     cout << "Choose the type of matrix (a or b):";
     cin >> exercise;
@@ -38,50 +36,59 @@ void function1(int *pointer, int N){
         int value = 1;
         for (int i = 0; i <= N / 2 - 1; i++){
             bool flag = false;
-            for (int x = i; x <= N - 1 - i; x++){
-                *(pointer + i*N + x) = value++;
+            for (int *x = pointer + i * N + i; x <= pointer + (i + 1) * N - i - 1; x++){
+                *x = value++;
                 flag = true;
             }
-            if(flag){ printMatrix(pointer, N); }
+            if(!flag){ break; }
+            printMatrix(pointer, N, end);
             flag = false;
-            for (int y = i + 1; y <= N - 1 - i; y++){
-                *(pointer + y*N + N - 1 - i) = value++;
+            for (int *y = pointer + (i + 2) * N - i - 1; y <= end - i * N - i; y += N){
+                *y = value++;
                 flag = true;
             }
-            if(flag){ printMatrix(pointer, N); }
+            if(!flag){ break; }
+            printMatrix(pointer, N, end);
             flag = false;
-            for (int z = N - 2 - i; z >= i; z--){
-                *(pointer + (N - 1- i)*N + z) = value++;
+            for (int *z = end - i * N - i - 1; z >= end - (i + 1) * N + i + 1; z--){
+                *z = value++;
                 flag = true;
             }
-            if(flag){ printMatrix(pointer, N); }
+            if(!flag){ break; }
+            printMatrix(pointer, N, end);
             flag = false;
-            for (int w = N - 2 - i; w >= i + 1; w--){
-                *(pointer + w*N + i) = value++;
+            for (int *w = end - (i + 2) * N + i + 1; w >= pointer + (i + 1) * N + i; w -= N){
+                *w = value++;
                 flag = true;
             }
-            if(flag){ printMatrix(pointer, N); }
+            if(!flag){ break; }
+            printMatrix(pointer, N, end);
         }
     } else if(exercise == 'b'){
         int value = 1;
         for (int i = 0; i <= N - 1; i++){
             bool flag = false;
-            for (int j = 0; j <= N - 1; j++){
-                *(pointer + j*N + i) = value++;
+            for (int *j = pointer + i; j <= end - N + i + 1; j += N){
+                *j = value++;
                 flag = true;
             }
             i++;
-            if(flag){ printMatrix(pointer, N); }
+            if(!flag){ break; }
+            printMatrix(pointer, N, end);
             flag = false;
-            for (int z = N - 1; z >= 0; z--){
-                *(pointer + z*N + i) = value++;
+            for (int *z = end - N + i + 1; z >= pointer + i; z -= N){
+                *z = value++;
                 flag = true;
             }
-            if(flag){ printMatrix(pointer, N); }
+            if(!flag){ break; }
+            printMatrix(pointer, N, end);
         }
     }else{
-        function1(pointer, N);
+        function1(pointer, N, end);
     }
+}
+//2.Получает новую матрицу, из матрицы п. 1, переставляя ее блоки в соответствии со схемами:
+void function2(){
 
 }
 
@@ -90,19 +97,19 @@ int main() {
     int N = checkInput();
     int arr[N][N];
     int *pointer = (int *) arr;
+    int *end = arr[0] + N * N - 1;
     //заполняем матрицу нулевыми значениями
-    for (int i = 0; i <= N - 1; i++){
-        for (int j = 0; j <= N - 1; j++){
-            *(pointer + i*N + j) = 0;
-        }
+    for (int *i = arr[0]; i <= end; i++){
+        *i = 0;
     }
-    printMatrix(pointer,N);
+    printMatrix(pointer, N, end);
     int circle = 1;
     while (circle == 1){
         //1.Используя арифметику указателей, заполняет квадратичную целочисленную матрицу порядка N (6,8,10) случайными числами от 1 до  N*N согласно схемам, приведенным на рисунках.
         // Пользователь должен видеть процесс заполнения квадратичной матрицы.
-        function1(pointer, N);
+        function1(pointer, N, end);
         //2.Получает новую матрицу, из матрицы п. 1, переставляя ее блоки в соответствии со схемами:
+        function2();
         cout << "if you want to repeat then input 1: ";
         circle = checkInput();
     }
